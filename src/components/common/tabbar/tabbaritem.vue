df0abc9412d62a3f1f5fbca7daf8cb64ba35ec74
unknown <271556543@qq.com>
2021-04-25 21:32:48 +0800

    ''

---
+++ src/components/common/tabbar/tabbaritem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="tabbar-item" @click="itemClick">
+    <div v-if="!isActive"><slot name="tabbaricon"></slot></div>
+    <div v-else><slot name="tabbaricon-active"></slot></div>
+    <div :style="activeStyle"><slot name="tabbername"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tabbaritem",
+  props:{
+    path:String,
+    activeColor:{
+      type:String,
+      default:'blue'
+    }
+  },
+  data(){
+    return{
+      // isActive:true
+    }
+  },
+  computed:{
+    isActive(){
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle(){
+      return this.isActive ? {color:this.activeColor} : {}
+    }
+  },
+  methods:{
+    itemClick(){
+      this.$router.push(this.path)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tabbar-item{
+  flex:1;
+  text-align: center;
+  height: 45px;
+  font-size: 12px;
+}
+.tabbar-item img{
+  weight:24px;
+  height:24px;
+  margin-top: 2px;
+  vertical-align: middle;
+}
+
+</style>
---
+++ src/components/content/mainTabbar/maintabbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <tabbar>
+    <tabbaritem path="/home" activeColor="red">
+      <img src="@/assets/img/tabbar/首页.png" alt="" slot="tabbaricon">
+      <img src="@/assets/img/tabbar/首页点击.png" alt="" slot="tabbaricon-active">
+      <div slot="tabbername">首页</div>
+    </tabbaritem>
+    <tabbaritem path="/category" >
+      <img src="@/assets/img/tabbar/分类.png" alt="" slot="tabbaricon">
+      <img src="@/assets/img/tabbar/分类点击.png" alt="" slot="tabbaricon-active">
+      <div slot="tabbername">分类</div>
+    </tabbaritem>
+    <tabbaritem path="/cart" >
+      <img src="@/assets/img/tabbar/购物车.png" alt="" slot="tabbaricon">
+      <img src="@/assets/img/tabbar/购物车点击.png" alt="" slot="tabbaricon-active">
+      <div slot="tabbername">购物车</div>
+    </tabbaritem>
+    <tabbaritem path="/profile" >
+      <img src="@/assets/img/tabbar/个人中心.png" alt="" slot="tabbaricon">
+      <img src="@/assets/img/tabbar/个人中心点击.png" alt="" slot="tabbaricon-active">
+      <div slot="tabbername">个人中心</div>
+    </tabbaritem>
+  </tabbar>
+
+</template>
+
+<script>
+import tabbar from '@/components/common/tabbar/tabbar'
+import tabbaritem from "@/components/common/tabbar/tabbaritem";
+export default {
+name: "maintabbar",
+  components:{
+    tabbar,
+    tabbaritem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
